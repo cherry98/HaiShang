5c4b74461451163ba0d25fdf8a30fec175c03beb
dongyan.xie <Dongyan.Xie@partner.bmw.com>
2019-05-11 17:30:54 +0800

    init_2

--- app/src/main/java/com/example/haishangzuoye/activity/SettingActivity.java
+++ app/src/main/java/com/example/haishangzuoye/activity/SettingActivity.java
@@ -50,5 +50,6 @@ public class SettingActivity extends BaseActivity {
 
     public void button_loginout(View view) {
         SharedPreferencesUtils.setLoggedStatus(this, false);
+        finish();
     }
 }
